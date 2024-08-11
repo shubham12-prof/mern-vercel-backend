8ded8f05004e0dfa3843474d1acb8d8b33360329
shubham12-prof <shubhsemwal12@gmail.com>
2024-08-11 20:37:54 +0530

    mongodb changes

--- seed.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Course = require('./models/Course');
-require('dotenv').config();
-
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
